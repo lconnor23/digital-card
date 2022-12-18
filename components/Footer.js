77b01626f68766a60b6038fea0ae30e90f7c5df7
Laura Connor <itslauraroseco@gmail.com>
2022-12-17 19:26:09 -0500

    fixed broken link

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer(){
     return (
         <div className = "footer">
-            <a href = "www.linkedin.com/in/itslaurarose"><i class="fa-brands fa-linkedin fa-2x"></i></a>
+            <a href = "https://www.linkedin.com/in/itslaurarose/"><i class="fa-brands fa-linkedin fa-2x"></i></a>
             <a href = "https://github.com/lconnor23"><i class="fa-brands fa-square-github fa-2x"></i></a>
             <a href = "https://twitter.com/itslauraroseco"><i class="fa-brands fa-square-twitter fa-2x"></i></a>
         </div>
